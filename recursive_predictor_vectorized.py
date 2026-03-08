Potential fix for normalization with NaN handling.

recursive_predictor_vectorized.py
@@ -331,7 +331,10 @@ def create_new_snapshot_vectorized(prev_snapshot, new_particle_state, current_ti
     sdf_gradients = SDF_gradient_vectorized(positions, moved_mesh)
 
     norms = np.linalg.norm(sdf_gradients, axis=1, keepdims=True)
-    normalized_gradients = np.where(norms > data_utils.EPSILON, sdf_gradients, sdf_gradients / norms)
+    # if norms value is nan change it to 1
+    norms = np.where(np.isnan(norms), 1.0, norms)
+
+    normalized_gradients = np.where(norms < data_utils.EPSILON, sdf_gradients, sdf_gradients / norms)
     sdf_distance_vectors = sdf_vals * normalized_gradients
 
     new_snapshot["particle"]["sdf_values"] = sdf_vals