0d45248bb46d8eec9e2dc326b96fe40fdf5637ef
kgergov <go98fod@mytum.de>
2026-03-10 13:39:33 +0100

    Training from Google Drive

--- recursive_predictor_vectorized.py
+++ recursive_predictor_vectorized.py
@@ -30,9 +30,9 @@ RPM = 3.1415926535 # Wall rotational speed (used for wall features)
 
 # Model hyperparameters
 WINDOW_SIZE = 5
-HIDDEN_DIM = 128
-MLP_LAYERS = 2
-INTERACTION_LAYERS = 10
+HIDDEN_DIM = 64
+MLP_LAYERS = 3
+INTERACTION_LAYERS = 6
 NODE_OUT_DIM = 3
 GLOBAL_OUT_DIM = 1
 EDGE_IN_DIM = 3 # Based on distance_vector and sdf_distance_vectors being 3D
