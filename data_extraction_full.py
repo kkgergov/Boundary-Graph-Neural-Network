38b5a7b92465bb6f87ff3d85ff0006f68db0202c
kgergov <go98fod@mytum.de>
2026-03-11 19:19:03 +0100

    Trimmed raw data for DyAOR training.
    Renamed old extracted_data to full, from now on we will use trimmed
    version of it for training stored in extracted_data_trimmed.
    Now the angle of rotation of the drum is saved for each timestep.

--- data_extraction_full.py
+++ data_extraction_full.py
@@ -51,31 +51,6 @@ def SDF_normal_direct_vectorized(points, target_mesh):
     normalized_normals = np.where(norms < 1e-8, normals, normals / norms)
     return normalized_normals
 
-# This is SLOW
-# def SDF_gradient_direct(point, target_mesh, epsilon=1e-5):
-#     """
-#     Computes the gradient of the SDF at a given point using central finite differences.
-    
-#     Args:
-#         point (array-like): A point in space (3D coordinates).
-#         target_mesh: A trimesh object.
-#         epsilon (float): A small perturbation for finite difference computation.
-        
-#     Returns:
-#         grad (np.array): The gradient vector of the SDF at the given point.
-#     """
-#     point = np.array(point).reshape(1, 3)
-#     grad = np.zeros(3)
-#     for i in range(3):
-#     # Create perturbation along the i-th axis.
-#         d = np.zeros(3)
-#         d[i] = epsilon
-#         # Compute SDF at slightly displaced points.
-#         sdf_plus = -trimesh.proximity.signed_distance(target_mesh, (point + d).reshape(1, 3))[0]
-#         sdf_minus = -trimesh.proximity.signed_distance(target_mesh, (point - d).reshape(1, 3))[0]
-#         grad[i] = (sdf_plus - sdf_minus) / (2 * epsilon)
-#     return grad
-
 def SDF_gradient_direct_vectorized(points, target_mesh, epsilon=1e-5):
     """
     Vectorized computation of the SDF gradient for multiple points using central finite differences.
@@ -103,23 +78,6 @@ def SDF_gradient_direct_vectorized(points, target_mesh, epsilon=1e-5):
     
     return grad
 
-
-###############################################
-# --- 3. Define COM Method for Moving the Mesh --- #
-###############################################
-
-# def get_moved_mesh_using_CoM(timestep_index, deck):
-#     com_edem = np.array(deck.timestep[timestep_index].geometry['First try jar'].getCoM())
-#     com_static = mesh_static.center_mass
-#     offset = com_edem - com_static
-#     moved_mesh = mesh_static.copy()
-#     moved_mesh.vertices = moved_mesh.vertices + offset
-#     return moved_mesh
-
-# def SDF_global_moved(x, timestep_index, deck):
-#     moved_mesh = get_moved_mesh_using_CoM(timestep_index, deck)
-#     return SDF_static(x, moved_mesh)
-
 # timestep_size = 0.01, rad_s = 1.0472 old
 def get_rotated_mesh(timestep_index, timestep_size = 0.001, rad_s = 3.1415926535, axis = [0, 0, 1], center=[0.44, 0.44, 0.0]):
     
@@ -144,27 +102,8 @@ def get_rotated_mesh(timestep_index, timestep_size = 0.001, rad_s = 3.1415926535
 #     return SDF_static(x, rotated_mesh)
 
 ###############################################
-# --- 4. Load EDEM Data and Define Snapshot Range --- #
+# --- 4. Load Rocky Data and Define Snapshot Range --- #
 ###############################################
-
-# decks = []
-# for fileName in glob.glob("*.dem"):
-#     decks.append(fileName)
-# if len(decks) == 0:
-#     print("No deck file found in the directory")
-#     sys.exit()
-# deck_file = decks[0]
-# deck = Deck(deck_file)
-# print("Using deck file:", deck_file)
-
-# start_timestep = 50000   # starting snapshot index
-# last_timestep  = 100000   # adjust as needed for your extraction range
-# total_timesteps = deck.numTimesteps
-# print("Total timesteps in simulation:", total_timesteps)
-
-# custom_properties_dict = deck.creatorData.simulationPropertyData
-# custom_properties_name = np.array(deck.creatorData.simulationCustomPropertyNames)
-# custom_properties_num = deck.creatorData.numSimulationCustomProperties
 
 # Load deck from pkl file
 with open("./data-drum/1-staging/rocky_deck.pkl", "rb") as f:
@@ -183,22 +122,8 @@ def extract_data(deck, start_timestep, last_timestep):
     prev_joint_tangential = None
     for t in range(start_timestep, last_timestep + 1):
         snapshot = {}
-        # snapshot["time"] = deck.timestepValues[t]
         snapshot["time"] = t * 0.001  # assuming timestep size of 0.001s
         snapshot["timestep"] = t  # store snapshot index
-        
-        # --- Particle Data ---
-        # particle = {}
-        # particle["num_particles"] = deck.timestep[t].particle[0].getNumParticles()
-        # particle["ids"] = deck.timestep[t].particle[0].getIds()
-        # particle["positions"] = np.array(deck.timestep[t].particle[0].getPositions())
-        # particle["velocities"] = np.array(deck.timestep[t].particle[0].getVelocities())
-        # particle["angular_velocities"] = np.array(deck.timestep[t].particle[0].getAngularVelocities())
-        # particle["mass"] = np.array(deck.timestep[t].particle[0].getMass())
-        # particle["net_forces"] = np.array(deck.timestep[t].particle[0].getForce())
-        # particle["torques"] = np.array(deck.timestep[t].particle[0].getTorque())
-        # particle["radius"] = np.array(deck.timestep[t].particle[0].getSphereRadii())
-        # particle["inertia"] = np.array((2/5) * particle["mass"] * (particle["radius"]**2))
 
         particle = {}
         particle["num_particles"] = deck.n_particles
@@ -212,15 +137,11 @@ def extract_data(deck, start_timestep, last_timestep):
         particle["net_forces"] = deck.p_net_forces[t]
         particle["torques"] = deck.p_net_torques[t]
 
-        # particle["sdf_values"] = SDF_global_rotated(particle["positions"], t)
-
         moved_mesh = get_rotated_mesh(t)
-        particle["sdf_values"] = SDF_static(particle["positions"], moved_mesh)
-        
-        # particle["sdf_normals"] = np.array([SDF_normal_direct(pos, moved_mesh) for pos in particle["positions"]])
-        particle["sdf_normals"] = SDF_normal_direct_vectorized(particle["positions"], moved_mesh)
 
-        # particle["sdf_gradients"] = np.array([SDF_gradient_direct(pos, moved_mesh) for pos in particle["positions"]]) # SLOW but accurate
+        # --- Particle Features ---
+        particle["sdf_values"] = SDF_static(particle["positions"], moved_mesh)
+        particle["sdf_normals"] = SDF_normal_direct_vectorized(particle["positions"], moved_mesh)
         particle["sdf_gradients"] = SDF_gradient_direct_vectorized(particle["positions"], moved_mesh)
 
         # Ensure sdf_values is a column vector (shape: [N, 1])
@@ -238,21 +159,11 @@ def extract_data(deck, start_timestep, last_timestep):
         # Compute the distance vectors.
         particle["sdf_distance_vectors"] = sdf_values * normalized_gradients
         
-    
-        
-        
         snapshot["particle"] = particle
         
         # --- Particle-Particle Contacts ---
         contacts_pp = {}
         try:
-            # contacts_pp["contact_ids"] = np.array(deck.timestep[t].contact.surfSurf.getIds()) # Available
-            # contacts_pp["normal_forces"] = np.array(deck.timestep[t].contact.surfSurf.getNormalForce()) # Available
-            # contacts_pp["tangential_forces"] = np.array(deck.timestep[t].contact.surfSurf.getTangentialForce()) # Available
-            # contact_vectors = np.array(deck.timestep[t].contact.surfSurf.getContactVector1())  # Shape (N, 3)
-            # contacts_pp["relative_distances"] = np.linalg.norm(contact_vectors, axis=1)  # Shape (N,)
-            # contacts_pp["distance_vector"] = np.array(deck.timestep[t].contact.surfSurf.getContactVector1()) * 2
-
             contacts_pp["contact_ids"] = deck.pp_c_ids[t]
             contacts_pp["normal_forces"] = deck.pp_c_fn[t]
             contacts_pp["tangential_forces"] = deck.pp_c_ft[t]
@@ -269,10 +180,6 @@ def extract_data(deck, start_timestep, last_timestep):
         # --- Particle-Wall Contacts ---
         contacts_pw = {}
         try:
-            # contacts_pw["normal_forces"] = np.array(deck.timestep[t].contact.surfGeom.getNormalForce()) # Available
-            # contacts_pw["tangential_forces"] = np.array(deck.timestep[t].contact.surfGeom.getTangentialForce()) # Available
-            # contacts_pw["contact_ids"] = np.array(deck.timestep[t].contact.surfGeom.getIds()[:, 0]) # Available, take only first column(particle ID that makes a contact with the wall)
-
             contacts_pw["contact_ids"] = deck.pw_c_ids[t][:, 0]
             contacts_pw["normal_forces"] = deck.pw_c_fn[t]
             contacts_pw["tangential_forces"] = deck.pw_c_ft[t]
@@ -285,24 +192,14 @@ def extract_data(deck, start_timestep, last_timestep):
         snapshot["contacts_particle_wall"] = contacts_pw
         
         # --- Wall Data ---
-        # moved_bounds = moved_mesh.bounds
-        # wall_center = np.array([
-        #     (moved_bounds[0, 0] + moved_bounds[1, 0]) / 2.0,
-        #     (moved_bounds[0, 1] + moved_bounds[1, 1]) / 2.0,
-        #     (moved_bounds[0, 2] + moved_bounds[1, 2]) / 2.0
-        # ])
-        # wall_rotational_speed = 300.0  # placeholder value
-        # wall_com = np.array(deck.timestep[t].geometry['First try jar'].getCoM())
         wall_rotational_speed = 3.1415926535 # radians
-        wall_com = [0.44, 0.44, 0.0]
+        wall_com = [0.44, 0.44, 0.125]
         wall_node_features = np.concatenate([wall_com, np.array([wall_rotational_speed])])
         snapshot["wall_node_features"] = wall_node_features
         
         # --- Energy Dissipation ---
         # For normal energy loss:
         try:
-            # pw_n_loss = deck.timestep[t].customProperties[np.where(custom_properties_name=='Particle-Wall Normal Energy Loss')[0][0]].getData()[0]
-            # pp_n_loss = deck.timestep[t].customProperties[np.where(custom_properties_name=='Particle-Particle Normal Energy Loss')[0][0]].getData()[0]
             pw_n_loss = deck.pw_n_loss[t]
             pp_n_loss = deck.pp_n_loss[t]
             joint_normal = pw_n_loss + pp_n_loss
@@ -311,8 +208,6 @@ def extract_data(deck, start_timestep, last_timestep):
         
         # For tangential energy loss:
         try:
-            # pw_t_loss = deck.timestep[t].customProperties[np.where(custom_properties_name=='Particle-Wall Tangential Energy Loss')[0][0]].getData()[0]
-            # pp_t_loss = deck.timestep[t].customProperties[np.where(custom_properties_name=='Particle-Particle Tangential Energy Loss')[0][0]].getData()[0]
             pw_t_loss = deck.pw_t_loss[t]
             pp_t_loss = deck.pp_t_loss[t]
             joint_tangential = pw_t_loss + pp_t_loss
---
+++ data_extraction_trimmed_DyAOR.py
@@ -0,0 +1,111 @@
+import numpy as np
+import pickle
+
+from scipy import stats
+
+DRUM_ROT_SPEED = 3.1415926535
+
+# --- Take a Z slice of the particle positions and use it to calculate Dynamic Angle of Repose (DyAOR) as a global feature for each timestep
+# Take a Z slice of the particle positions and use it to calculate Dynamic Angle of Repose (DyAOR) as a global feature for each timestep
+def calculate_DyAOR(positions, wall_node_features, nbins=1000):
+
+    # Get the Z slice of particle positions (e.g., particles within a certain Z range around the drum's mid-plane)
+    z_min = wall_node_features[2] - 0.1 # Center Z minus particle 3*diameter as margin
+    z_max = wall_node_features[2] + 0.1 # Center Z plus particle 3*diameter as margin
+    slice_mask = (positions[:, 2] >= z_min) & (positions[:, 2] <= z_max)
+    positions_slice = positions[slice_mask]
+
+    if len(positions_slice) < 10:
+        raise ValueError("Too few particles in the selected slice.")
+
+    x = positions_slice[:, 0]
+    y = positions_slice[:, 1]
+
+    # Bin the X coordinates
+    x_min, x_max = x.min(), x.max()
+    bin_edges = np.linspace(x_min, x_max, nbins + 1)
+    bin_indices = np.digitize(x, bins=bin_edges) - 1  # 0-based bins
+
+    surface_x = []
+    surface_y = []
+    for i in range(nbins):
+        in_bin = (bin_indices == i)
+        if np.any(in_bin):
+            # Find the highest particle in this bin
+            y_bin = y[in_bin]
+            max_idx = np.argmax(y_bin)
+            # Record its actual coordinates (not the bin centre)
+            x_bin = x[in_bin]
+            surface_x.append(x_bin[max_idx])
+            surface_y.append(y_bin[max_idx])
+
+    if len(surface_x) < 3:
+        raise ValueError("Not enough surface points for a reliable fit.")
+
+    surface_x = np.array(surface_x)
+    surface_y = np.array(surface_y)
+
+    # Linear regression
+    slope, intercept, r_value, p_value, std_err = stats.linregress(surface_x, surface_y)
+
+    # Angle in degrees (absolute value, orientation depends on rotation direction)
+    angle = np.arctan(np.abs(slope)) * 180.0 / np.pi
+    return angle
+
+# --- Trimmed extracted data only for ML training ---
+# We get rid of everything except particle positions, and we also trim the number of timesteps to 1000
+
+# Load extracted data
+with open("./data-drum/1-staging/extracted_data.pkl", "rb") as f:
+    extracted_data = pickle.load(f)
+
+# Trim the number of timesteps to 1000
+extracted_data = extracted_data[:1000]
+
+trimmed_data = []
+for snapshot_full in extracted_data:
+
+    snapshot_trimmed = {}
+    particle_trimmed = {}
+    contacts_pp_trimmed = {}
+    contacts_pw_trimmed = {}
+
+    snapshot_trimmed["time"] = snapshot_full["time"]
+    snapshot_trimmed["timestep"] = snapshot_full["timestep"]
+
+    # --- Get only needed particle features ---
+
+    particle_full = snapshot_full["particle"]
+    particle_trimmed["ids"] = particle_full["ids"]
+    particle_trimmed["positions"] = particle_full["positions"]
+    particle_trimmed["velocities"] = particle_full["velocities"]
+    particle_trimmed["net_forces"] = particle_full["net_forces"] # This will be used as ground truth for training, so we keep it in the trimmed data.
+    particle_trimmed["sdf_values"] = particle_full["sdf_values"]
+    particle_trimmed["sdf_gradients"] = particle_full["sdf_gradients"]
+    particle_trimmed["sdf_distance_vectors"] = particle_full["sdf_distance_vectors"]
+
+    snapshot_trimmed["particle"] = particle_trimmed
+
+    # --- Get only needed contact features ---
+    contacts_pp_full = snapshot_full["contacts_particle_particle"]
+    contacts_pp_trimmed["contact_ids"] = contacts_pp_full["contact_ids"]
+    contacts_pp_trimmed["distance_vector"] = contacts_pp_full["distance_vector"]
+    snapshot_trimmed["contacts_particle_particle"] = contacts_pp_trimmed
+
+    contacts_pw_full = snapshot_full["contacts_particle_wall"]
+    contacts_pw_trimmed["contact_ids"] = contacts_pw_full["contact_ids"]
+    snapshot_trimmed["contacts_particle_wall"] = contacts_pw_trimmed
+
+    snapshot_trimmed["wall_node_features"] = snapshot_full["wall_node_features"]
+
+    snapshot_trimmed["wall_node_features"][2] = 0.125 # Drum CoM Z axis fix
+    snapshot_trimmed["wall_node_features"][3] = DRUM_ROT_SPEED * snapshot_trimmed["time"] # rad/s * time = current angle of the drum
+
+    # Our second ground truth for training, DyAOR is a global feature that we want the model to predict
+    snapshot_trimmed["AOR"] = calculate_DyAOR(snapshot_trimmed["particle"]["positions"], snapshot_trimmed["wall_node_features"])
+    trimmed_data.append(snapshot_trimmed)
+
+# Save trimmed data in compressed npz format
+output_file = "./data-drum/1-staging/extracted_data_trimmed.npz"
+np.savez_compressed(output_file, data=trimmed_data)
+print(f"Trimmed data saved to {output_file}")
